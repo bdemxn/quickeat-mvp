079e04db4c34c4af5d48e935fe8b6f07580ff45e
Kevin Bonilla <kb1909zzz@gmail.com>
2026-05-22 15:44:47 -0600

    add logic for register form

--- src/components/auth/register-form.tsx
+++ src/components/auth/register-form.tsx
@@ -1,31 +1,139 @@
 "use client";
 
+import { useForm } from "@tanstack/react-form";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 import { Button } from "@/components/ui/button";
-import { Field, FieldContent, FieldLabel } from "@/components/ui/field";
+import {
+	Field,
+	FieldContent,
+	FieldError,
+	FieldLabel,
+} from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
+import { useAuthClient } from "@/hooks/use-auth-client";
+import { registerSchema } from "@/shared/validations";
 
 export function RegisterForm() {
+	const { signUpWithEmailAndPassword } = useAuthClient();
+	const router = useRouter();
+
+	const form = useForm({
+		defaultValues: {
+			fullname: "",
+			email: "",
+			password: "",
+		},
+		validators: {
+			onSubmit: registerSchema,
+		},
+		onSubmit: ({ value }) =>
+			toast.promise(
+				async () =>
+					await signUpWithEmailAndPassword(
+						value.email,
+						value.password,
+						value.fullname,
+					),
+				{
+					success: (data) => {
+						router.push("/");
+						return `Bienvenido ${data.user.name}`;
+					},
+					error: (error) => {
+						return `Error: ${error.message}`;
+					},
+				},
+			),
+	});
+
 	return (
-		<form className="flex w-full max-w-sm flex-col gap-6">
+		<form
+			className="flex w-full max-w-sm flex-col gap-6"
+			onSubmit={(event) => {
+				event.preventDefault();
+				form.handleSubmit();
+			}}
+		>
 			<div className="flex flex-col gap-4">
-				<Field>
-					<FieldLabel>Full Name</FieldLabel>
-					<FieldContent>
-						<Input type="text" placeholder="Enter your full name" />
-					</FieldContent>
-				</Field>
-				<Field>
-					<FieldLabel>Email</FieldLabel>
-					<FieldContent>
-						<Input type="email" placeholder="Enter your email" />
-					</FieldContent>
-				</Field>
-				<Field>
-					<FieldLabel>Password</FieldLabel>
-					<FieldContent>
-						<Input type="password" placeholder="Enter your password" />
-					</FieldContent>
-				</Field>
+				<form.Field
+					name="fullname"
+					children={(field) => {
+						const isInvalid =
+							field.state.meta.isTouched && !field.state.meta.isValid;
+
+						return (
+							<Field>
+								<FieldLabel>Full Name</FieldLabel>
+								<FieldContent>
+									<Input
+										id={field.name}
+										name={field.name}
+										value={field.state.value}
+										onBlur={field.handleBlur}
+										onChange={(e) => field.handleChange(e.target.value)}
+										placeholder="Pon tu nombre"
+										type="text"
+										required
+									/>
+									{isInvalid && <FieldError errors={field.state.meta.errors} />}
+								</FieldContent>
+							</Field>
+						);
+					}}
+				/>
+				<form.Field
+					name="email"
+					children={(field) => {
+						const isInvalid =
+							field.state.meta.isTouched && !field.state.meta.isValid;
+
+						return (
+							<Field>
+								<FieldLabel>Email</FieldLabel>
+								<FieldContent>
+									<Input
+										id={field.name}
+										name={field.name}
+										value={field.state.value}
+										onBlur={field.handleBlur}
+										onChange={(e) => field.handleChange(e.target.value)}
+										placeholder="Pon tu nombre"
+										type="email"
+										required
+									/>
+									{isInvalid && <FieldError errors={field.state.meta.errors} />}
+								</FieldContent>
+							</Field>
+						);
+					}}
+				/>
+				<form.Field
+					name="password"
+					children={(field) => {
+						const isInvalid =
+							field.state.meta.isTouched && !field.state.meta.isValid;
+
+						return (
+							<Field>
+								<FieldLabel>Password</FieldLabel>
+								<FieldContent>
+									<Input
+										id={field.name}
+										name={field.name}
+										value={field.state.value}
+										onBlur={field.handleBlur}
+										onChange={(e) => field.handleChange(e.target.value)}
+										placeholder="Digite su contraseña"
+										type="password"
+										required
+									/>
+									{isInvalid && <FieldError errors={field.state.meta.errors} />}
+								</FieldContent>
+							</Field>
+						);
+					}}
+				/>
 			</div>
 			<div className="flex flex-col gap-3">
 				<Button type="submit" className="w-full">
